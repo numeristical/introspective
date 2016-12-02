928007a05b4ca347bb92da85c133dbadfc03b27f
Lucenator <lucenator@Lucenators-MacBook-Pro.local>
2016-12-01 17:41:22 -0800

    Added prob calibration

--- ml_insights/__init__.py
+++ ml_insights/__init__.py
@@ -3,8 +3,8 @@ Package Docuemntation
 """
 # -*- coding: utf-8 -*-
 
-from .insights import ModelXRay
+from .insights import ModelXRay, prob_calibration_function
 
-__all__ = ["ModelXRay"]
+__all__ = ["ModelXRay","prob_calibration_function"]
 
 __version__ = '0.0.7'
--- ml_insights/insights.py
+++ ml_insights/insights.py
@@ -375,3 +375,58 @@ def explain_prediction_difference(model, data_row_1, data_row_2, tol=.03, verbos
         feat_val_change_list.append((old_feat_val, new_feat_val))
     return feat_list, feat_val_change_list, move_list, val_list
 
+def _natural_cubic_spline_basis_expansion(xpts,knots):
+    num_knots = len(knots)
+    num_pts = len(xpts)
+    outmat = np.zeros((num_pts,num_knots))
+    outmat[:,0]= np.ones(num_pts)
+    outmat[:,1] = xpts
+    def make_func_H(k):
+        def make_func_d(k):
+            def func_d(x):
+                denom = knots[-1] - knots[k-1]
+                numer = np.maximum(x-knots[k-1],np.zeros(len(x)))**3 - np.maximum(x-knots[-1],np.zeros(len(x)))**3
+                return numer/denom
+            return func_d
+        def func_H(x):
+            d_fun_k = make_func_d(k)
+            d_fun_Km1 = make_func_d(num_knots-1)
+            return d_fun_k(x) -  d_fun_Km1(x)
+        return func_H    
+    for i in range(1,num_knots-1):
+        curr_H_fun = make_func_H(i)
+        outmat[:,i+1] = curr_H_fun(xpts)
+    return outmat
+
+def prob_calibration_function(scorevec, truthvec,  reg_param_vec='default',
+                                knots = 'all', extrapolate=True):
+    """This function takes an uncalibrated set of scores and the true 0/1 values and returns a calibration function.
+
+    This calibration function can then be applied to other scores from the same model and will return an accurate probability
+    based on the data it has seen.  For best results, the calibration should be done on a separate validation set (not used
+    to train the model).
+    """
+    from sklearn import linear_model
+    from sklearn.metrics import log_loss, make_scorer
+    
+    knot_vec = np.unique(scorevec)    
+    X_mat = _natural_cubic_spline_basis_expansion(scorevec, knot_vec)   
+
+    
+    if ((type(reg_param_vec)==str) and (reg_param_vec=='default')):
+        reg_param_vec = 10**np.linspace(-6,6,241)
+    print("Trying {} values of C between {} and {}".format(len(reg_param_vec),np.min(reg_param_vec),np.max(reg_param_vec)))
+    reg = linear_model.LogisticRegressionCV(Cs=reg_param_vec, cv=5, scoring=make_scorer(log_loss,needs_proba=True, greater_is_better=False))
+    reg.fit(X_mat, truthvec)
+    print("Best value found C = {}".format(reg.C_))
+            
+    def calibrate_scores(new_scores):
+        if (not extrapolate):
+            new_scores = np.maximum(new_scores,knot_vec[0]*np.ones(len(new_scores)))
+            new_scores = np.minimum(new_scores,knot_vec[-1]*np.ones(len(new_scores)))
+        basis_exp = _natural_cubic_spline_basis_expansion(new_scores,knot_vec)
+        outvec = reg.predict_proba(basis_exp)[:,1]
+        return outvec
+    return calibrate_scores
+
+
