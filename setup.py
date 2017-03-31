98734b7d66be4130955f5a2ff525cbc8a8ff9a38
blucena <brianlucena@gmail.com>
2017-03-31 11:18:11 -0700

    setup change

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setup(
     name='ml_insights',
-    version='0.0.14',
+    version='0.0.15',
     description="Package to understand ML Models",
     long_description=readme,
     author="Ramesh Sampath / Brian Lucena",
